e6ac23a40012fe85c52d44505925aace98c0d594
Vibhor Saini <Vibhorsaini14@gmail.com>
2018-06-14 11:38:52 -0700

    assingment submited

---
+++ vibhor03.py
@@ -0,0 +1,11 @@
+#(3). Take the input of 3 variables x,y and print their value of screen. 
+
+x = int(input("Enter first no.: "))
+y = int(input("Enter second no.: "))
+z = int(input("Enter third no.: "))
+s = x+y+z
+print(x)
+print(y)
+print(z)
+print(s)
+print(type(s))
