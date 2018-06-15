7aca12170cc172ef96a59cc7926fc6623932810b
Vibhor Saini <Vibhorsaini14@gmail.com>
2018-06-14 22:48:17 -0700

    git commands

--- Assignment01.py.py
+++ Assignment01.py.py
@@ -1,5 +1,21 @@
-# (1).print anything you want on screen.
-print("write a program")
-print('Hello Python')
-print("""write a program""")
-					
+#    BASIC FUNCTIONS	   'and'		GIT COMMANDS	   
+ 
+(1). Initialize Git:->                  $ git init
+
+(2). Set Everything to commit:->        $ git add.
+
+(3). Set Single File to commit:->       $ git add index.py
+
+(4). Show the Working tree status:->    $ git status
+
+(5). Create Alias for user Name:->		$ git config --global user.name'Vibhorsaini'
+
+(6). Create Alias for user Email:->		$ git config --global user.email'vibhorsaini88mail.com'
+
+(7). Commit Changes:->					$ git commit -m "COMMITMESSAGE"
+
+(8). Show Commits:->               		$ git log
+
+(9). Show Remote Branches:-> 			$ git remote add origin "http:\\link"
+
+(10).Show Push Branches:->				$ git push origin master
