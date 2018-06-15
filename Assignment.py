a6f6cf78e76efec2267f68fa1c94d5068e5e0d6a
Vibhor Saini <Vibhorsaini14@gmail.com>
2018-06-14 19:56:02 -0700

    filecommit

---
+++ Assignment.py
@@ -0,0 +1,21 @@
+#    BASIC FUNCTIONS	   'and'		GIT COMMANDS	   
+ 
+(1). Initialize Git:->                  git init
+
+(2). Set Everything to commit:->        git add.
+
+(3). Set Single File to commit:->       git add index.py
+
+(4). Show the Working tree status:->    git status
+
+(5). Create Alias for user Name:->		git config --global user.name'Vibhorsaini'
+
+(6). Create Alias for user Email:->		git config --global user.email'vibhorsaini88mail.com'
+
+(7). Commit Changes:->					git commit -m "COMMITMESSAGE"
+
+(8). Show Commits:->               		git log
+
+(9). Show Remote Branches:-> 			git remote add origin "http:\\link"
+
+(10).Show Push Branches:->				git push origin master
