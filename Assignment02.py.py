ebee0b8552fe8d6452681f22f8423f30f1b5b54b
Vibhor Saini <Vibhorsaini14@gmail.com>
2018-06-15 00:17:25 -0700

    second commited

--- Assignment02.py.py
+++ Assignment02.py.py
@@ -1,4 +1,10 @@
-#(2).Join two string using '+'.
+#(Q.1)- Print anything you want on screen. 
+
+print("Hello world")
+print("print the Informations")
+
+#(Q.2)- Join two strings using '+'. 
+# E.g.-"Acad"+"View"
 
 p = "Acad"
 q = "view"
@@ -7,109 +13,40 @@ r = str(p+q)
 print(r)
 print(type(r))
 
-#(3). Take the input of 3 variables x,y and print their value of screen. 
+#(Q.3)- Take the input of 3 variables x, y and z . Print their values on screen.
 
 x = int(input("Enter first no.: "))
 y = int(input("Enter second no.: "))
 z = int(input("Enter third no.: "))
-s = x+y+z
 print(x)
 print(y)
 print(z)
 print(s)
 print(type(s))
 
-#(4). Print "let's get started" on screen,using "\" operator.
+#(Q.4)- Print "let's get started" on screen.
                   
 v = "let\'s get started"
 print(v)
 print(type(v))
 
-q = "Hello \n World"
-print(q)
+#(Q.5)- Print the following values using placeholders.
+#s=”Acadview”
+#course=”Python”
+#fees=5000
 
-p = "\'Hello\'\'world\'"
-print(p)
+s='Acadview'
+course='Python'
+fees=5000
 
-r = "Hello \t world"
-print(r)
+print("%s %s %d"%(s,course,fees))
 
-x = 15
-y = 2.5
-z = 'c'
-print(x,y,z,sep="-",end="!")
-print("Hello World")
+#(Q.6)- Let’s do some interesting exercise:
+#name=”Tony Stark”
+#salary=1000000
+#print(‘%s’’%d’)%(____ ,____) 
 
-
-#(5). print the following values using "placeholder".
-                    
-x = 20
-y = 3.5
-z = 'vib'
-print("%d %f %s"%(x,y,z))
-print("%d+%F=%s"%(x,y,z))
-print(x,"+",y,"=",z)
-
-s      = "Acadview"
-course = "Python"
-fees   =  6000
-print(s,"\n")
-print(course,"\n")
-print(type(course))
-print(fees)
-print(type(fees))
+name = "Tony Stark"
+salary = 1000000
+print("%s %d"%(name,salary))
                    
-#(6). String handling.
-                 
-x = "v i b h o r"
-print(len(x))
-                 
-y = "python"
-print(y.upper())
-print(y.lower())				   
-
-z = " world "
-print(z.strip(),end=" ")
-print("Hello")
-
-p = "Hello"
-print(p.isalpha())
-                 
-q = "16088"
-print(q.isdigit())
-                 
-r = " "
-print(r.isspace())
-                  
-s = "Python"
-print(s.startswith("P"))
-                    
-t = "Python"
-print(t.endswith("n"))
-
-u = "world"
-print(u.find("r"))
-v = "Pythan"
-print(v.replace("a","o"))
-
-w = "H,E,L,o"
-print(w.split(","))
-				   
-
-#(7). String word store with position(01234...)
-# Eg:-    WORLD
-#       +(01234)
-#       -(54321)
-#For(+)-> 
-x ="WORLD"
-print(x[1:4]) #is'ORL'
-print(x[1:])  #is'ORLD'
-print(x[:])   #is'WORLD'
-print(x[1:100])#is'ORLD'
-                   
-# For(-)
-print(x[-1]) #is'D'
-print(x[-4]) #is'O'
-print(x[:-3])#is'WO'
-print(x[-3:])#is'RLD'  
-                
